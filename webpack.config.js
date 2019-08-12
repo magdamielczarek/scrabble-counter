339dcc4ce4b56d178b431cb663a5c27605d809f0
Magda Mielczarek <mielczarek.research@gmail.com>
2019-08-12 15:59:41 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = ({mode}) => {
+  console.log(mode);
+  return {
+    entry: './src/js/index.js',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js',
+      publicPath: '../../'
+    },
+    mode,
+    watch: true,
+    resolve:
+        { extensions: ['.js', '.ts'] },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 8080,
+      inline: true,
+      publicPath: '/',
+      watchContentBase: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader'
+          }
+        }, {
+          test: /\.(png|jp(e*)g|gif|woff|woff2|eot|ttf|svg)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/',
+                useRelativePath: true,
+              }
+            },
+            {
+              loader: 'image-webpack-loader',
+            }
+          ]
+        }, {
+          test: /\.(s)css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },{
+              loader: "css-loader",
+            },{
+              loader: "resolve-url-loader",
+            },{
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true,
+                plugins: () => [
+                  autoprefixer
+                ]
+              },
+            },{
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }, {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: true
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      })
+    ]
+  }
+};
