4ce363d848c14a3413ad11b2a1618accb8192ba5
Magda Mielczarek <mielczarek.research@gmail.com>
2019-08-19 19:30:40 +0200

    fix config

--- src/js/GameBoard.js
+++ src/js/GameBoard.js
@@ -87,7 +87,7 @@ export default class GameBoard {
 
         // add bonuses for each letter and for whole word
         wordElement.innerHTML += `<div class="word-bonus">
-                <button class="delete-word-btn"><img src="../assets/svg/delete.svg"/></button>
+                <button class="delete-word-btn"><img src="../../dist/assets/delete.svg"/></button>
                 <div class="bonus__container">
                      <input id=${word.id}x2 data-word-bonus="x2" data-word-id=${word.id} type="checkbox">
                      <label for=${word.id}x2 class="bonus__button bonus__button--x2word">x2</label>
